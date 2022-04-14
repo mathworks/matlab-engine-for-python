d3c62c2e35b474c363e2fdf245a4bd10301ab471
Dakota Junkman <djunkman@mathworks.com>
2022-04-14 14:50:18 -0500

    update name in setup.py

--- setup.py
+++ setup.py
@@ -278,7 +278,7 @@ if __name__ == '__main__':
         long_description = rm.read()
 
     setup(
-        name="matlabengine-final",
+        name="matlabengine",
         # MUST_BE_UPDATED_EACH_RELEASE (Search repo for this string)
         version="9.6",
         description='A module to call MATLAB from Python',
