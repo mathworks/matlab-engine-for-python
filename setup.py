4254566c59d8bbc1b00c6efe0c61a9597616b08f
AlanF-MW <103284994+alanfmw@users.noreply.github.com>
2022-09-12 15:10:55 -0400

    R2021a 9.10.1a3

--- setup.py
+++ setup.py
@@ -21,10 +21,10 @@ class _MatlabFinder(build_py):
     }
     
     # MUST_BE_UPDATED_EACH_RELEASE (Search repo for this string)
-    MATLAB_REL = 'R2021b'
+    MATLAB_REL = 'R2021a'
 
     # MUST_BE_UPDATED_EACH_RELEASE (Search repo for this string)
-    MATLAB_VER = '9.10.1a0' 
+    MATLAB_VER = '9.10.1a3' 
 
     # MUST_BE_UPDATED_EACH_RELEASE (Search repo for this string)
     SUPPORTED_PYTHON_VERSIONS = set(['3.7', '3.8'])
@@ -309,7 +309,7 @@ if __name__ == '__main__':
     setup(
         name="matlabengine",
         # MUST_BE_UPDATED_EACH_RELEASE (Search repo for this string)
-        version="9.10.1a0",
+        version="9.10.1a3",
         description='A module to call MATLAB from Python',
         author='MathWorks',
         license="MathWorks XSLA License",
