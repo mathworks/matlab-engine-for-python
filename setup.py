0b4f01b459d2de4720ebc8e3641a671170924073
AlanF-MW <103284994+alanfmw@users.noreply.github.com>
2022-10-06 19:23:25 -0400

    fixed comment; version number is 9.13.2

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ class _MatlabFinder(build_py):
     MATLAB_REL = 'R2022b'
 
     # MUST_BE_UPDATED_EACH_RELEASE (Search repo for this string)
-    MATLAB_VER = '9.13.1'
+    MATLAB_VER = '9.13.2'
 
     # MUST_BE_UPDATED_EACH_RELEASE (Search repo for this string)
     SUPPORTED_PYTHON_VERSIONS = set(['3.8', '3.9', '3.10'])
@@ -208,8 +208,7 @@ class _MatlabFinder(build_py):
     def verify_matlab_release(self, root):
         """
         Parses VersionInfo.xml to verify the MATLAB release matches the supported release
-        for the Python Engine. The major and minor version numbers must match. Everything
-        else will be ignored.
+        for the Python Engine.
         """
         version_info = os.path.join(root, 'VersionInfo.xml')
         if not os.path.isfile(version_info):
@@ -309,7 +308,7 @@ if __name__ == '__main__':
     setup(
         name="matlabengine",
         # MUST_BE_UPDATED_EACH_RELEASE (Search repo for this string)
-        version="9.13.1",
+        version="9.13.2",
         description='A module to call MATLAB from Python',
         author='MathWorks',
         license="MathWorks XSLA License",
