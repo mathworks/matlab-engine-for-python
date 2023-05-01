484dc9b1fbd80ff0d8d4ae6dddf376865b8d34f2
alexjose <alexjose@MATHWORKS-ITQ2V.local>
2023-05-01 16:18:50 -0400

    Add tests

---
+++ test/tInstall.m
@@ -0,0 +1,41 @@
+classdef tInstall < matlab.unittest.TestCase
+% Verify installation of matlab engine
+
+% Copyright 2023 Mathworks, Inc.
+
+    properties (Constant)
+        MATLABVersion = string(ver('MATLAB').Version)
+        MATLABRelease = erase(ver('MATLAB').Release,{'(',')'})
+    end
+
+    methods (Test)
+        function installNoVersionSpecified(testCase)
+            assumeEqual(testCase, testCase.MATLABRelease, 'R2023a')
+            [status, out] = system("pip install matlabengine");
+            verifyEqual(testCase, status, 0, out)
+            verifyInstallation(testCase)
+        end
+
+        function installMatchingEngine(testCase)
+            [status, out] = system("pip install matlabengine==" + testCase.MATLABVersion + ".*");
+            verifyEqual(testCase, status, 0, out)
+            verifyInstallation(testCase)
+        end
+    end
+
+    methods
+        function verifyInstallation(testCase)
+        % Verify installation by calling functions in matlab engine
+        % Share this session and see if find_matlab can find it.
+            sharedEngineName = string(matlab.engine.engineName);
+            if (sharedEngineName == "")
+                sharedEngineName = "MATLAB_tInstall";
+                matlab.engine.shareEngine(sharedEngineName)
+            end
+            pySharedEngineName = string(py.matlab.engine.find_matlab());
+            verifyEqual(testCase, pySharedEngineName, sharedEngineName)
+            system("pip uninstall -y matlabengine")
+        end
+    end
+end
+
