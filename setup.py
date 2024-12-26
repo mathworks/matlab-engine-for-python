2ded919b19a01b6610825feef8e83d7b25e73fcb
AlanF-MW <103284994+alanfmw@users.noreply.github.com>
2024-12-26 13:45:17 -0500

    removed patch number from suggested commands in readme

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ class _MatlabFinder(build_py):
     MATLAB_REL = 'R2024b'
 
     # MUST_BE_UPDATED_EACH_RELEASE (Search repo for this string)
-    MATLAB_VER = '24.2.1'
+    MATLAB_VER = '24.2.2'
 
     # MUST_BE_UPDATED_EACH_RELEASE (Search repo for this string)
     SUPPORTED_PYTHON_VERSIONS = set(['3.9', '3.10', '3.11', '3.12'])
@@ -417,7 +417,7 @@ if __name__ == '__main__':
     setup(
         name="matlabengine",
         # MUST_BE_UPDATED_EACH_RELEASE (Search repo for this string)
-        version="24.2.1",
+        version="24.2.2",
         description='A module to call MATLAB from Python',
         author='MathWorks',
         license="LICENSE.txt, located in this repository",
